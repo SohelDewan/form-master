9d68d4d51d476eb506136b1b9425ca5a217b98f8
SohelDewan <sohelds241@gmail.com>
2024-03-21 22:59:16 +0600

    deal with useRef and useEffect

--- src/__form__.js
+++ src/__form__.js
@@ -1,5 +1,8 @@
 /****
  * 1. use form tag and onSubmit handler with event(e)
  * Form e access e.target.[name of the input field].value
- * 2. use individual field State
+ * 2. use individual field State to contorle elements
+ * 3. use controled elements L: one obejct holding all the fields values
+ * 4. uncontroled elements : by reference hook useRef to create reference to
+ * the element and access value by using like:nameRef.current.value
  */
